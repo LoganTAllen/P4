5fa93561bdf44f2c520b6ccdfe3293b66ecaf40c
parkernilson <nilsopar@gmail.com>
2019-02-06 14:30:26 -0700

    implement method stubs
    implement method stubs in the AnagramChecker.java file

--- src/assign04/AnagramChecker.java
+++ src/assign04/AnagramChecker.java
@@ -1,5 +1,99 @@
 package assign04;
 
+import java.util.Comparator;
+
 public class AnagramChecker {
+
+
+	/**
+	 * This method returns the lexicographically-sorted version of
+	 * the input string
+	 * 
+	 * @param str - the input string
+	 * @return - the lexicographically sorted version of the input string
+	 */
+	public static String sort(String str) {
+		
+		return "";
+	}
+	
+	/**
+	 * This generic method sorts the input array using an insertion sort
+	 * and the input Comparator object
+	 * 
+	 * @param arr - the array to be sorted
+	 * @param comparator - the comparator to use for comparing
+	 */
+	public static <T> void insertionSort(T[] arr, Comparator<? super T> comparator) {
+		
+	}
+	
+	/**
+	 * Returns true if the two input strings are anagrams of eachother
+	 * 
+	 * @param str0 - First String
+	 * @param str1 - Second String
+	 * @return - whether the two input strings are anagrams of eachother
+	 */
+	public static boolean areAnagrams(String str0, String str1) {
+		
+		return false;
+	}
+	
+	/**
+	 * This method returns the largest group of anagrams in the input
+	 * array of words, in no particular order.
+	 * It returns an empty array if there are no anagrams in the input array.
+	 * 
+	 * @param arr - the array to check for anagrams in 
+	 * @return - an array of the largest group of anagrams contained in the input array
+	 */
+	public static String[] getLargestAnagramGroup(String[] arr) {
+		
+		return null;
+	}
+	
+	/**
+	 * This method returns the largest group of anagrams contained in the input
+	 * file.
+	 * The file is assumed to contain a single word per line.
+	 * returns an empty array if the file does not exist or is empty.
+	 * 
+	 * @param filename - the path to the file to search for anagrams
+	 * @return - an array of the largest group of anagrams contained in the file.
+	 */
+	public static String[] getLargestAnagramGroup(String filename) {
+		
+		return null;
+	}
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
